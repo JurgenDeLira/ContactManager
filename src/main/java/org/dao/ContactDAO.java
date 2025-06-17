962f874d160746553858f8bb3c44f9689beda233
JurgenDeLira <arq.jorgedelira@gmail.com>
2025-06-17 15:44:53 -0700

    1

---
+++ src/main/java/org/dao/ContactDAO.java
@@ -0,0 +1,44 @@
+package org.dao;
+
+import org.model.Contact;
+import org.util.DBConnection;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactDAO {
+    public void save(Contact contact){
+        String sql = "INSERT INTO contacts(name, email, phone) VALUES (?, ?, ?)";
+        try (Connection conn = DBConnection.getConnection();
+        PreparedStatement stmt = conn.prepareStatement(sql)){
+            stmt.setString(1, contact.getName());
+            stmt.setString(2, contact.getEmail());
+            stmt.setString(3, contact.getPhone());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Contact> contacts= new ArrayList<>();
+    String sql = "SELECT * FROM contacts";
+
+    try (Connection conn = DBConnection.getConnection();
+    Statement stmt = conn.createStatement();
+    ResultSet rs = stmt.executeQuery(sql)) {
+
+        while (rs.next()) {
+            Contact c = new Contact(
+                    rs.getInt("id"),
+                    rs.getString("name"),
+                    rs.getString("email"),
+                    rs.getString("phone")
+            );
+            contacts.add(c);
+        }
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+    return contacts;
+}
